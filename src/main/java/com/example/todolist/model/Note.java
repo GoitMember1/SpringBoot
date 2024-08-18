2fd1dfd2632eee67797704066c17d513a5c18bd6
MrRomashkaaa <mrromashkaofficial@gmail.com>
2024-08-18 21:22:58 +0200

    update

--- src/main/java/com/example/todolist/model/Note.java
+++ src/main/java/com/example/todolist/model/Note.java
@@ -1,6 +1,18 @@
 package com.example.todolist.model;
 
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity
 public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String title;
     private String content;
